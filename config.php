461b14978bbc4c5c5c1a5c4da428afe7128d2df3
Simone Ponte <sponte91@gmail.com>
2023-10-04 17:54:01 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,10 +5,10 @@ class DbConn
       private $connection;
       private static $instance;
 
-      private $_host = 'localhost';
-      private $_username = 'root';
-      private $_password = '';
-      private $_database = 'docebotest';
+      private $_host = DBHOST;
+      private $_username = DBUSR ;
+      private $_password = DBPWD;
+      private $_database = DBSCHEMA;
       private $options = [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET utf8"
